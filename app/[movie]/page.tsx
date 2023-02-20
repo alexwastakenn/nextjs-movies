87b060479a422d679a6db86278439c32cd1f70ba
alexwastakenn <112557932+alexwastakenn@users.noreply.github.com>
2023-02-20 10:24:23 +0100

    .

--- app/[movie]/page.tsx
+++ app/[movie]/page.tsx
@@ -5,6 +5,7 @@ export async function generateStaticParams() {
     `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
   )
   const res = await data.json()
+
   return res.results.map((movie: any) => ({
     movie: toString(movie.id),
   }))
@@ -12,29 +13,32 @@ export async function generateStaticParams() {
 
 export default async function MovieDetail({ params }: any) {
   const { movie } = params
-  const imagePath = 'https://image.tmdb.org/t/p/original/'
+  const imagePath = 'https://image.tmdb.org/t/p/original'
   const data = await fetch(
-    `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`,
-    { next: { revalidate: 0 } }
+    `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`
   )
   const res = await data.json()
+
   return (
     <div>
-      <div>
-        <h2 className='text-2xl'>{res.title}</h2>
-        <h2 className='text-lg'>{res.release_date}</h2>
-        <h2 className=''> {res.runtime} minutes</h2>
-        <h2 className='text-sm bg-green-600 inline-block my-2 py-2 px-4 rounded-md'>
+      <div className=''>
+        <h2 className='text-4xl'>{res.title}</h2>
+        <h1 className='text-lg '>{res.release_date}</h1>
+        <h2>Runtime: {res.runtime} minutes</h2>
+        <h2 className='bg-green-600 inline-block my-2 py-2 px-4 rounded-lg text-sm'>
           {res.status}
         </h2>
         <Image
-          className='my-12'
+          className='my-12 w-full'
           src={imagePath + res.backdrop_path}
+          alt={res.title}
           width={1000}
           height={1000}
-          alt={res.title}
+          priority
         />
-        <p>{res.overview}</p>
+      </div>
+      <div className='my-4'>
+        <p className='text-lg'>{res.overview}</p>
       </div>
     </div>
   )
